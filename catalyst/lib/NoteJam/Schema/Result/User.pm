ea5be85c8456ca5bc4d6a60df97c7f198ccaf2c7
Hans Staugaard <hstaugaard@users.noreply.github.com>
2015-11-22 14:19:41 +0100

    Catalyst: Automatic password hashing

--- catalyst/lib/NoteJam/Schema/Result/User.pm
+++ catalyst/lib/NoteJam/Schema/Result/User.pm
@@ -4,11 +4,18 @@ use strict;
 use warnings;
 use parent 'DBIx::Class::Core';
 
+__PACKAGE__->load_components(qw/EncodedColumn/);
 __PACKAGE__->table('users');
 __PACKAGE__->add_columns(
     id       => {data_type => 'integer', is_auto_increment => 1},
     email    => {data_type => 'varchar', size => 75},
-    password => {data_type => 'text', size => 128},
+    password => {
+        data_type     => 'text',
+        size          => 128,
+        encode_column => 1,
+        encode_class  => 'Digest',
+        encode_args   => {algorithm => 'SHA-1', format => 'hex'},
+    },
 );
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint('email_unique', ['email']);
